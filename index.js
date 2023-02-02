59dc584c50c16ebab583299af88d89f74f027304
Greg Morrison <freedomtoroam@posteo.de>
2023-02-02 11:11:06 +1000

    sitemate technical interview submission

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
-// expecting time to be a string in the format like '8:15' or '12:30'
-function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
-
-  return 'half past eight';
+const numberToWords = {
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "quarter",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  21: "twenty one",
+  22: "twenty two",
+  23: "twenty three",
+  24: "twenty four",
+  25: "twenty five",
+  26: "twenty six",
+  27: "twenty seven",
+  28: "twenty eight",
+  29: "twenty nine",
+  30: "half",
 }
 
-module.exports = { convertTimeToWords };
+// expecting time to be a string in the format like '8:15' or '12:30'
+function convertTimeToWords(time) {
+  // TODO: extend error handling via checking string formatting conforms to pattern
+  if (typeof time !== "string") {
+    throw new Error(`${time} is not a string type argument`)
+  }
+
+  if (checkForMidnight(time)) {
+    return "midnight"
+  }
+
+  const [hourStr, minutesStr] = getHourAndMinutesAsIntFromTimeString(time)
+  const hourInt = parseInt(hourStr)
+  const minutesInt = parseInt(minutesStr)
+
+  if (minutesInt > 30) {
+    const nextHour = hourInt + 1
+    const minutesFromNextHour = 60 - minutesInt
+
+    return `${numberToWords[minutesFromNextHour]} to ${numberToWords[nextHour]}`
+  } else if (minutesInt === 0) {
+    return `${numberToWords[hourInt]} o'clock`
+  }
+
+  return `${numberToWords[minutesInt]} past ${numberToWords[hourInt]}`
+}
+
+function getHourAndMinutesAsIntFromTimeString(timeStr) {
+  return timeStr.split(":")
+}
+
+function checkForMidnight(timeStr) {
+  return timeStr === "0:00" ? true : false
+}
+
+module.exports = { convertTimeToWords, numberToWords }
--- index.test.js
+++ index.test.js
@@ -1,18 +1,49 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index")
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
-  });
+describe("Time to words", () => {
+  describe("Functional arguemnts", () => {
+    it("should throw an error for non-string type arguments", () => {
+      const arg = 5
+      expect(() => convertTimeToWords(arg)).toThrow()
+    })
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
-  });
+    it("should not throw an error for string type arguments", () => {
+      const arg = "eight thirty"
+      expect(() => convertTimeToWords(arg)).not.toThrow()
+    })
+  })
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
-  });
-});
+  describe("Hour translation", () => {
+    it("should translate the integer value to its appropriate human-readable string value", () => {
+      const arg = "1:00"
+
+      expect(convertTimeToWords(arg)).toEqual("one o'clock")
+    })
+  })
+
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00")
+    expect(timeInWords).toBe("midnight")
+  })
+
+  it("Handles 30 - 8:30", () => {
+    const timeInWords = convertTimeToWords("8:30")
+    expect(timeInWords).toBe("half past eight")
+  })
+
+  it("Handles times after 30 mins", () => {
+    const timeInWords = convertTimeToWords("2:45")
+    expect(timeInWords).toBe("quarter to three")
+
+    const nonIntervalicTime = convertTimeToWords("2:53")
+    expect(nonIntervalicTime).toBe("seven to three")
+  })
+
+  it("Handles times before 30 mins", () => {
+    const timeInWords = convertTimeToWords("2:15")
+    expect(timeInWords).toBe("quarter past two")
+
+    const nonIntervalicTime = convertTimeToWords("2:28")
+    expect(nonIntervalicTime).toBe("twenty eight past two")
+  })
+})
